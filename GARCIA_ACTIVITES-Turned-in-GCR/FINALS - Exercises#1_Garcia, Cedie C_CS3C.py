e724439742e988fb006aa3e0f216a179eebaf15c
Kappine <125634246+CedieGarcia@users.noreply.github.com>
2024-04-28 23:09:55 -0700

    Add files via upload

---
+++ GARCIA_ACTIVITES-Turned-in-GCR/FINALS - Exercises#1_Garcia, Cedie C_CS3C.py
@@ -0,0 +1,17 @@
+print("FINALS - Exercise #1_Garcia, Cedie C._CS3C\n")
+
+print("\nThe Output for number 1.[i for i in citcs]\n")
+result = [i for i in "citcs"]
+print(result)
+
+print("\nThe Output for number 2.[i for i in range(1,11)]\n")
+result = [i for i in range(1, 11)]
+print(result)
+
+print("\nThe Codes for number 3.[2, 4, 6, 8, 10]\n")
+result = [i for i in range(2, 11, 2)]
+print(result)
+
+print("\nThe Codes for number 4.[1, 3, 5, 7 , 9]\n")
+result = [i for i in range(1, 10, 2)]
+print(result)
